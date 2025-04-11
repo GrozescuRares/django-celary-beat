9f62a3bd5c9a501c719f4fb0d678f83cc06ebbcb
Grozescu Rares <raresionut.grozescu@concentrix.com>
2025-04-11 17:25:35 +0300

    F-1: Resolved celary tasks.

--- project/core/settings.py
+++ project/core/settings.py
@@ -16,6 +16,8 @@ from pathlib import Path
 
 from celery.schedules import crontab
 
+import core.tasks # noqa: F401
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
