b9979d95daf5a3700e4a614beb1df4c233692246
Grozescu Rares <raresionut.grozescu@concentrix.com>
2025-04-12 02:32:50 +0300

    F-9&F11: Create task api and create celary task for processing an operation.

--- project/core/tasks.py
+++ project/core/tasks.py
@@ -1,18 +1,32 @@
-from celery import shared_task
-from celery.utils.log import get_task_logger
-from django.core.management import call_command
+import time
 
+from celery.utils.log import get_task_logger
+
+from core.celery import app
 
 logger = get_task_logger(__name__)
 
 
-@shared_task
-def sample_task():
-    logger.info("The sample task just ran.")
+@app.task
+def process_task(task_id: int) -> None:
+    from tasks.models import Task, TaskStatus
+    # TODO should this be wrapped into an atomic transaction?
+    task = Task.objects.get(task_id=task_id)
+    task.status = TaskStatus.STARTED
+    task.save()
+    logger.info(f"Start processing task with id: {task_id} which has priority: {task.priority}")
 
+    time.sleep(20)
 
-@shared_task
-def send_email_report():
-    call_command(
-        "email_report",
-    )
+    try:
+        operands = task.operation.split("+")
+        result = sum(float(operand) for operand in operands)
+
+        task.result = result
+        task.status = TaskStatus.SUCCESS
+        logger.info(f"Task {task_id} completed successfully with result: {result}")
+    except Exception as e:
+        task.status = TaskStatus.ERROR
+        logger.error(f"Error processing task {task_id}: {e}")
+    finally:
+        task.save()
--- project/tasks/views.py
+++ project/tasks/views.py
@@ -1,10 +1,13 @@
 from django.db import transaction
+from django.db.transaction import on_commit
 from rest_framework import viewsets, mixins, status
 from rest_framework.request import Request
 from rest_framework.response import Response
 
 from tasks.models import Task
 from tasks.serializers import TaskSerializer
+
+from core.tasks import process_task
 
 
 class TaskViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
@@ -13,7 +16,8 @@ class TaskViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
 
     def perform_create(self, serializer) -> None:
         with transaction.atomic():
-            serializer.save()
+            task = serializer.save()
+            on_commit(lambda: process_task.apply_async(args=[task.task_id], priority=task.priority))
 
     def create(self, request: Request, *args, **kwargs) -> Response:
         response = super().create(request, *args, **kwargs)
